5205d24dd6d9d8ebfcf543bc51800e95ffecfcdb
Ryan <ryanisaacg@users.noreply.github.com>
2024-07-16 16:06:50 -0400

    Fix piano roll being backwards

--- frontend/src/piano.tsx
+++ frontend/src/piano.tsx
@@ -8,40 +8,40 @@ interface PianoRollProps {
 export function PianoRoll({ play }: PianoRollProps) {
   return (
     <ul class="set">
-      <li class="white b" onClick={() => play("B")}>
+      <li class="white c" onClick={() => play("C")}>
         <p>A</p>
       </li>
-      <li class="black as" onClick={() => play("A#")}>
+      <li class="black cs" onClick={() => play("C#")}>
         <p>W</p>
       </li>
-      <li class="white a" onClick={() => play("A")}>
+      <li class="white d" onClick={() => play("D")}>
         <p>S</p>
       </li>
-      <li class="black gs" onClick={() => play("G#")}>
+      <li class="black ds" onClick={() => play("D#")}>
         <p>E</p>
       </li>
-      <li class="white g" onClick={() => play("G")}>
+      <li class="white e" onClick={() => play("E")}>
         <p>D</p>
-      </li>
-      <li class="black fs" onClick={() => play("F#")}>
-        <p>R</p>
       </li>
       <li class="white f" onClick={() => play("F")}>
         <p>F</p>
       </li>
-      <li class="white e" onClick={() => play("E")}>
+      <li class="black fs" onClick={() => play("F#")}>
+        <p>R</p>
+      </li>
+      <li class="white g" onClick={() => play("G")}>
         <p>G</p>
       </li>
-      <li class="black ds" onClick={() => play("D#")}>
+      <li class="black gs" onClick={() => play("G#")}>
         <p>Y</p>
       </li>
-      <li class="white d" onClick={() => play("D")}>
+      <li class="white a" onClick={() => play("A")}>
         <p>H</p>
       </li>
-      <li class="black cs" onClick={() => play("C#")}>
+      <li class="black as" onClick={() => play("A#")}>
         <p>U</p>
       </li>
-      <li class="white c" onClick={() => play("C")}>
+      <li class="white b" onClick={() => play("B")}>
         <p>J</p>
       </li>
     </ul>
