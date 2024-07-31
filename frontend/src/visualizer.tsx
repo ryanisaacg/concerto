993d60a46845cfa7ab7a3cec8e4e8d872bd8b414
Ryan <ryanisaacg@users.noreply.github.com>
2024-07-31 17:00:59 -0400

    Fix coordinates not matching the earth

--- frontend/src/visualizer.tsx
+++ frontend/src/visualizer.tsx
@@ -24,31 +24,36 @@ export function Visualizer() {
       <div>
         <button
           onClick={() =>
-            addNote.current?.(new Note(-40.69, -73.98, -RADIUS, 0x00ffaa))
+            addNote.current?.(new Note(40.69, -73.98, -RADIUS, 0x00ffaa))
           }
         >
           Brooklyn
         </button>
         <button
           onClick={() =>
-            addNote.current?.(new Note(-48.86, 2.35, -RADIUS, 0x00ffaa))
+            addNote.current?.(new Note(48.86, 2.35, -RADIUS, 0x00ffaa))
           }
         >
           Paris
         </button>
         <button
           onClick={() =>
-            addNote.current?.(new Note(-28.7, 77.1, -RADIUS, 0x00ffaa))
+            addNote.current?.(new Note(28.7, 77.1, -RADIUS, 0x00ffaa))
           }
         >
           New Delhi
         </button>
         <button
           onClick={() =>
-            addNote.current?.(new Note(37.81, 144.96, -RADIUS, 0x00ffaa))
+            addNote.current?.(new Note(-37.81, 144.96, -RADIUS, 0x00ffaa))
           }
         >
           Melbourne
+        </button>
+        <button
+          onClick={() => addNote.current?.(new Note(0, 0, -RADIUS, "red"))}
+        >
+          Null Island
         </button>
       </div>
     </div>
@@ -161,55 +166,18 @@ function createCircle(radius: number) {
   return circleGeometry;
 }
 
-export function latLongToVec(lat: number, long: number): THREE.Vector3 {
-  // Adapted from https://stackoverflow.com/questions/10473852/convert-latitude-and-longitude-to-point-in-3d-space
-  /*
-   *     f  = 0                              # flattening
-    ls = atan((1 - f)**2 * tan(lat))    # lambda
-
-    x = rad * cos(ls) * cos(lon) + alt * cos(lat) * cos(lon)
-    y = rad * cos(ls) * sin(lon) + alt * cos(lat) * sin(lon)
-    z = rad * sin(ls) + alt * sin(lat)
-
-    return c4d.Vector(x, y, z)
-   */
-  const latRadians = (lat * 2 * Math.PI) / 180;
-  const longRadians = (long * Math.PI) / 180;
-  const x =
-    Math.cos(longRadians) + Math.cos(latRadians) * Math.cos(longRadians);
-  const z =
-    Math.sin(longRadians) + Math.cos(latRadians) * Math.sin(longRadians);
-  const y = Math.sin(latRadians);
-
-  return new THREE.Vector3(x, y, z);
-}
-
 export function latLongToQuat(lat: number, long: number): THREE.Quaternion {
   // Adapted from https://stackoverflow.com/questions/5437865/longitude-latitude-to-quaternion
   const radLat = (lat * Math.PI) / 180;
   const q1 = new THREE.Quaternion();
-  q1.setFromAxisAngle(new THREE.Vector3(0, 0, 1), radLat);
+  q1.setFromAxisAngle(new THREE.Vector3(1, 0, 0), radLat);
 
-  const radLong = (long * Math.PI) / 180;
+  // This -90 compensates for the Earth not matching our home-baked coordinate system
+  const radLong = ((long - 90) * Math.PI) / 180;
   const q2 = new THREE.Quaternion();
   q2.setFromAxisAngle(new THREE.Vector3(0, 1, 0), radLong);
 
-  return q1.multiply(q2);
-}
-
-function lineSegment(
-  start: THREE.Vector3,
-  end: THREE.Vector3,
-  color: THREE.ColorRepresentation,
-): THREE.Line {
-  const material = new THREE.LineBasicMaterial({ color });
-
-  const points = [];
-  points.push(start);
-  points.push(end);
-
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
-  return new THREE.Line(geometry, material);
+  return q2.multiply(q1);
 }
 
 class Note {
