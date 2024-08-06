1c3e3f59d486b0951cc392cc352e3b4ce6186526
Ryan <ryanisaacg@users.noreply.github.com>
2024-08-06 15:08:06 -0400

    Buffer up to 1024 old messages

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,15 @@
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::{
+    collections::VecDeque,
+    sync::{
+        atomic::{AtomicU32, Ordering},
+        Arc,
+    },
+};
 
 use futures_util::{SinkExt, StreamExt};
 use tokio::{
     net::{TcpListener, TcpStream},
-    sync::broadcast,
+    sync::{broadcast, RwLock},
 };
 use tokio_tungstenite::{accept_async, tungstenite::Message};
 
@@ -13,22 +19,38 @@ async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind(&addr).await?;
     eprintln!("Listening on 9003");
 
-    let (ping_sender, _) = broadcast::channel(1024);
+    let (ping_sender, mut buffer_recv) = broadcast::channel(1024);
+
+    let buffer: Arc<RwLock<VecDeque<(u32, String)>>> = Arc::new(RwLock::new(VecDeque::new()));
+
+    let buf = buffer.clone();
+    tokio::spawn(async move {
+        loop {
+            let message = buffer_recv.recv().await.unwrap();
+            let mut buf_write = buf.write().await;
+            buf_write.push_back(message);
+            if buf_write.len() > BUF_LEN_THRESHHOLD {
+                buf_write.pop_front();
+            }
+        }
+    });
 
     while let Ok((stream, _)) = listener.accept().await {
         let sender = ping_sender.clone();
-        tokio::spawn(async move { client_task(stream, sender).await.unwrap() });
+        let buffer = buffer.clone();
+        tokio::spawn(async move { client_task(stream, sender, buffer).await.unwrap() });
     }
 
     Ok(())
 }
 
-
+const BUF_LEN_THRESHHOLD: usize = 1024;
 static CLIENT_ID: AtomicU32 = AtomicU32::new(0);
 
 async fn client_task(
     stream: TcpStream,
     sender: broadcast::Sender<(u32, String)>,
+    old_message_buffer: Arc<RwLock<VecDeque<(u32, String)>>>,
 ) -> anyhow::Result<()> {
     let client_id = CLIENT_ID.fetch_add(1, Ordering::Relaxed);
 
@@ -37,6 +59,11 @@ async fn client_task(
     let ws_stream = accept_async(stream).await?;
     let (mut ws_send, mut ws_recv) = ws_stream.split();
 
+    for (sender_id, ping) in old_message_buffer.read().await.iter() {
+        if *sender_id != client_id {
+            ws_send.send(Message::Text(ping.clone())).await.unwrap();
+        }
+    }
 
     tokio::spawn(async move {
         loop {
@@ -57,4 +84,3 @@ async fn client_task(
         }
     }
 }
-
